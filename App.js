1bcf5541eba7d7c6760d36296b6368c990fc465b
Armedi <armedi@outlook.com>
2019-01-27 15:58:16 +0700

    improved accelerometer handling

--- App.js
+++ App.js
@@ -8,29 +8,14 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Home from './Containers/Home'
 
 export default class App extends Component {
-
-  state = {
-    height: 0,
-    width: 0,
-  }
-
   render() {
-    let { height, width } = Dimensions.get('window')
-
     return (
       <View style={styles.container}>
-        <Text style={{ fontSize: 30 }}>{this.state.height} ini Height</Text>
-        <Text style={{ fontSize: 30 }}>{this.state.width} ini Width</Text>
-
-        <Button
-          title="huehue"
-          onPress={() => this.setState({ height, width })}
-        />
-        <Home userId={this.state.userId} />
+        <Home />
       </View>
     );
   }
@@ -42,15 +27,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
--- Containers/Home/index.js
+++ Containers/Home/index.js
@@ -4,6 +4,7 @@ import { setUpdateIntervalForType, SensorTypes, accelerometer } from 'react-nati
 import firebase from 'react-native-firebase'
 
 const R = require('ramda')
+const G = 9.80665
 
 setUpdateIntervalForType(SensorTypes.accelerometer, 25);
 
@@ -11,12 +12,16 @@ export default class Home extends Component {
 
   state = {
     userId: 'user123',
+    acceleration: { x: 0, y: 0, z: 0 },
+    accelerationInclGravity: { x: 0, y: 0, z: 9.8 },
     g: { x: 0, y: 0, z: 1 },
     samples: [],
     jump: false,
   }
 
   _subscribeAccelerometer = () => {
+    let lastJump = Date.now()
+
     accelerometer.subscribe(({ x, y, z, timestamp }) => {
       if (Math.sqrt(x ** 2 + y ** 2 + z ** 2) <= 10) {
         this.setState({
@@ -28,19 +33,42 @@ export default class Home extends Component {
         })
       }
 
+      let accelerationInclGravity = { x, y, z }
+      let acceleration = {
+        x: x - this.state.g.x * G,
+        y: y - this.state.g.y * G,
+        z: z - this.state.g.z * G,
+      }
+
+      let quadraticSum =
+        Math.sign(this.state.g.x * acceleration.x) * (this.state.g.x * acceleration.x) ** 2
+        + Math.sign(this.state.g.y * acceleration.y) * (this.state.g.y * acceleration.y) ** 2
+        + Math.sign(this.state.g.z * acceleration.z) * (this.state.g.z * acceleration.z) ** 2
+
       this.setState(state => ({
-        samples: state.samples.slice(-19).concat(
-          Math.sqrt((state.g.x * x) ** 2 + (state.g.y * y) ** 2 + (state.g.z * z) ** 2)
-        )
+        accelerationInclGravity,
+        acceleration,
+        samples: state.samples.slice(-9).concat(
+          Math.sqrt(Math.abs(quadraticSum)) * Math.sign(quadraticSum)
+        ),
       }))
 
-      let jump = this.state.samples.length >=20 && R.mean(this.state.samples) > 11
+      let jump = this.state.samples.length >= 10 && R.mean(this.state.samples) > 5
       if (jump !== this.state.jump) {
         console.log('jump', jump)
         this.setState({ jump })
         firebase.database().ref(`/${this.state.userId}/jump`).set(jump)
       }
     })
+  }
+
+  handlePress = () => {
+    this.setState({ jump: true })
+    firebase.database().ref(`/${this.state.userId}/jump`).set(true)
+      .then(() => {
+        this.setState({ jump: false })
+        firebase.database().ref(`/${this.state.userId}/jump`).set(false)
+      })
   }
 
   // componentWillMount() {
@@ -58,10 +86,8 @@ export default class Home extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{ fontSize: 30 }}>{this.state.g.x.toFixed(5)} ini GX</Text>
-        <Text style={{ fontSize: 30 }}>{this.state.g.y.toFixed(5)} ini GY</Text>
-        <Text style={{ fontSize: 30 }}>{this.state.g.z.toFixed(5)} ini GZ</Text>
+      <View style={styles.container} onPress={this.handlePress} >
+        <Text style={styles.instruction}>Swing the phone up or tap the screen to jump</Text>
         {
           this.state.jump
           && <Text style={{ fontSize: 50 }}>Loncat</Text>
@@ -76,16 +102,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
+    width: '100%',
+    padding: 30
   },
-  welcome: {
+  instruction: {
+    color: 'white',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center"
+  }
 });
