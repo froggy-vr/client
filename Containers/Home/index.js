dc46f161fccf540c284943f2360cd3ce27620a30
arnoldherod <arnold.herod@gmail.com>
2019-01-28 14:29:05 +0700

    Fixed message and cleaning up styling

--- Containers/Home/index.js
+++ Containers/Home/index.js
@@ -27,7 +27,7 @@ export default class Home extends Component {
           onRead={this.onSuccess.bind(this)}
           topContent={
             <Text style={styles.centerText}>
-              Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+              Scan The Barcode To Start The Game
             </Text>
           }
         />
@@ -74,18 +74,7 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 18,
     padding: 32,
-    color: '#777',
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-  buttonTouchable: {
-    padding: 16,
+    color: '#ffffff',
   },
 });
 
