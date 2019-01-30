bba3593e72aa05ff2e3402bdf914d93da8786879
Armedi <armedi@outlook.com>
2019-01-30 17:27:13 +0700

    added gyroscope to increase accuracy

--- Containers/GameController/index.js
+++ Containers/GameController/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { setUpdateIntervalForType, SensorTypes, accelerometer } from 'react-native-sensors'
+import { setUpdateIntervalForType, SensorTypes, accelerometer, gyroscope } from 'react-native-sensors'
 import { StackActions, NavigationActions } from 'react-navigation';
 import firebase from 'react-native-firebase'
 import KeepAwake from 'react-native-keep-awake'
@@ -8,21 +8,30 @@ import KeepAwake from 'react-native-keep-awake'
 const G = 9.80665
 
 setUpdateIntervalForType(SensorTypes.accelerometer, 25);
+setUpdateIntervalForType(SensorTypes.gyroscope, 25);
+
+let gyroscopeSubscribtion
 let accelerometerSubscribtion
 
 export default class Home extends Component {
 
   state = {
-    jump: false,
+    jump: false
   }
 
-  _subscribeAccelerometer = () => {
+  _subscribeSensors = () => {
 
-    let g = { x: 0, y: 0, z: 1 }
+    let gyro = 0
     let lastUpwardAcceleration = 0
+    let g = { x: 0, y: 0, z: 1 }
+
+    gyroscopeSubscribtion = gyroscope.subscribe(({ x, y, z, timestamp }) => {
+      gyro = Math.sqrt(x ** 2 + y ** 2 + z ** 2)
+    })
 
     accelerometerSubscribtion = accelerometer.subscribe(({ x, y, z, timestamp }) => {
-      if (Math.sqrt(x ** 2 + y ** 2 + z ** 2) <= 10) {
+      let totalAccelerationInclGravity = Math.sqrt(x ** 2 + y ** 2 + z ** 2)
+      if (totalAccelerationInclGravity >= G - 0.2 && totalAccelerationInclGravity <= G + 0.2) {
         g = {
           x: Math.sin(Math.atan(x / Math.sqrt(y ** 2 + z ** 2))),
           y: Math.sin(Math.atan(y / Math.sqrt(x ** 2 + z ** 2))),
@@ -42,12 +51,17 @@ export default class Home extends Component {
         + Math.sign(g.z * acceleration.z) * (g.z * acceleration.z) ** 2
 
       let upwardAcceleration = Math.sqrt(Math.abs(quadraticSum)) * Math.sign(quadraticSum)
-      
-      if (upwardAcceleration - lastUpwardAcceleration > 5 && !this.state.jump) {
-        this.setState({ jump: true })
+
+      if (
+        upwardAcceleration > 7
+        && upwardAcceleration - gyro > 7
+        && upwardAcceleration - lastUpwardAcceleration > 5
+        && !this.state.jump
+      ) {
+        this.setState({jump: true})
         firebase.database().ref(`/${this.props.navigation.getParam('gameId')}/jump`).set(true)
       } else if (Math.abs(upwardAcceleration) < 0.2 && this.state.jump) {
-        this.setState({ jump: false })
+        this.setState({jump: false})
         firebase.database().ref(`/${this.props.navigation.getParam('gameId')}/jump`).set(false)
       }
 
@@ -74,11 +88,12 @@ export default class Home extends Component {
   }
 
   componentDidMount() {
-    this._subscribeAccelerometer()
+    this._subscribeSensors()
   }
 
   componentWillUnmount() {
     accelerometerSubscribtion.unsubscribe()
+    gyroscopeSubscribtion.unsubscribe()
   }
 
   render() {
